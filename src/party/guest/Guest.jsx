477d3e2d81dd1ce731dd4846fa6a9f775f7b9464
alinareabteva <52407474+alinareabteva@users.noreply.github.com>
2023-02-02 15:04:04 +0200

    created a new component guest

---
+++ src/party/guest/Guest.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import Input from "../components/input/Input";
+import "./Guest.css";
+
+const Guest = () => {
+
+    return (
+        
+            <div id="container">
+                
+
+            </div>
+
+            
+        
+    );
+}
+
+export default Guest;
