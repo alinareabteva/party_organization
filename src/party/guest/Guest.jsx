b0527e28a8a1de9cb4cc92ad6685b617788ac3f5
Alina Reabtev <alinareabteva@gmail.com>
2023-02-06 16:20:16 +0200

    modified Guest, add new logic to handleChange, add ref to scroll and new  styles

--- src/party/guest/Guest.jsx
+++ src/party/guest/Guest.jsx
@@ -1,18 +1,49 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef } from "react";
 import Input from "../components/input/Input";
-import "./Guest.css";
+import Select from "../components/select/Select";
+import Icon, { IconNames, IconSizes } from "../components/icon/icon";
+import { alcoholOptions } from "./Data";
+import "./Guest.scss";
 
-const Guest = () => {
+const Guest = ({ guest, index, handleGuestChange, onClickDelete, onAlcoholChange, isLastItem }) => {
+    const ref = useRef(null)
+    const handleChange = ({ target: { name, value } }) => {
+        handleGuestChange(index, name, value)
+    }
+
+    useEffect(() => {
+        if (isLastItem && ref.current) {
+            ref.current.scrollIntoView({
+                behavior: "smooth"
+            })
+        }
+    }, [ref, isLastItem])
 
     return (
-        
-            <div id="container">
-                
+        <div className="guest-wrapper" ref={ref}>
+            <div className="box">
+                <Icon onClick={() => onClickDelete(index)} size={IconSizes.medium} name={IconNames.Close} />
+                <div className="row">
+                    <Input name="firstName" label="First Name:" placeholder="First Name" type="text" value={guest.firstName} onChange={handleChange} />
+                </div>
+                <div className="row">
+                    <Input name="lastName" label="Last Name:" placeholder="Last Name" type="text" value={guest.lastName} onChange={handleChange} />
+                </div>
+                <div className="row">
+                    <Input name="birthDate" label="Birth Date:" type="date" value={guest.birthDate} onChange={handleChange} />
+                </div>
+                <div className="row sex">
+                    <label className="labelSex">Sex:</label>
+                    <Input name="sex" label="M" value="M" type="radio" checked={guest.sex === "M"} onChange={handleChange} />
+                    <Input name="sex" label="F" value="F" type="radio" checked={guest.sex === "F"} onChange={handleChange} />
+                </div>
+                <div className="row select">
+                    <label className="labelAlcohol">Alcohol:</label>
+                    <Select allowMultiple onChange={(alcohol) => onAlcoholChange(index, alcohol)} options={alcoholOptions} selectedOptions={guest.alcohol} />
+                </div>
 
             </div>
-
-            
-        
+        </div>
     );
 }
 
