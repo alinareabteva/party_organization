a4dd7ca6eae38c652b45c8260415e8a9883bcbd2
Alina Reabtev <alinareabteva@gmail.com>
2023-02-10 16:37:16 +0200

    add new component Submit Button

---
+++ src/party/submit_button/SubmitButton.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from "../components/button/Button";
+import "./SubmitButton.scss"
+
+const SubmitButton = () => {   
+    
+    return (
+        <div className="submitForm">
+        <Button>Submit</Button>
+    </div>
+    )
+}
+export default SubmitButton;
