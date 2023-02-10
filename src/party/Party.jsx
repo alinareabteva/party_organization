19bdf095a097cde3a68374b88c2e4f5fd62d0294
Alina Reabtev <alinareabteva@gmail.com>
2023-02-10 16:38:20 +0200

    add new components to Party: Modal and SubmitButton

--- src/party/Party.jsx
+++ src/party/Party.jsx
@@ -2,7 +2,10 @@ import React, { useState } from "react";
 import PartyOrganization from "./party_organization/PartyOrganization";
 import GuestList, { GUEST_INITIAL_STATE } from "./guest/GuestList";
 import Overview from "./overview/Overview";
+import SubmitButton from "./submit_button/SubmitButton";
+import Modal from "./components/modal/Modal";
 import "./Party.css";
+
 
 const usePartyState = () => {
     const [state, setState] = useState({
@@ -41,31 +44,81 @@ const usePartyState = () => {
         }))
 
     }
+
+    const openModal = () => {
+        setState(prevState => ({
+            ...prevState,
+            modal: {
+                open: true
+            }
+
+        }))
+    }
+
+    const handleSubmit = () => {
+        setState(prevState => ({
+            ...prevState,
+            modal: {
+                open: false
+            }
+
+        }))
+    }
+
+    const handleCancel = () => {
+        setState(prevState => ({
+            ...prevState,
+            modal: {
+                open: false
+            }
+        }))
+    }
+
+
     return {
         partyState: state,
         handlePartyOrganizationChange,
-        handleGuestChange
+        handleGuestChange,
+        openModal,
+        handleSubmit,
+        handleCancel
     }
 }
 
 const Party = () => {
     const {
-        partyState: {partyOrganization, guests},
+        partyState: { partyOrganization, guests, modal },
         handlePartyOrganizationChange,
-        handleGuestChange
+        handleGuestChange,
+        openModal,
+        handleSubmit,
+        handleCancel
     } = usePartyState();
 
     return (
         <>
             <form id="form">
                 <div id="container">
-                    <PartyOrganization handleChange={handlePartyOrganizationChange} partyOrganization={partyOrganization} />
-                    <GuestList guests={guests} onChangeGuests={handleGuestChange} />
+                    <PartyOrganization
+                        handleChange={handlePartyOrganizationChange}
+                        partyOrganization={partyOrganization} />
+                    <GuestList
+                        guests={guests}
+                        onChangeGuests={handleGuestChange} />
                     <Overview guests={guests} />
                 </div>
-                {/* submit Button */}
+                <SubmitButton onClick={openModal} />
             </form>
-            {/* modal */}
+            <Modal
+                title="The affirmation"
+                isOpen={openModal}
+                onCancel={handleCancel}
+                onSubmit={handleSubmit}
+            >
+                <p>
+                    glugufu
+                </p>
+            </Modal>
         </>
     );
 }
