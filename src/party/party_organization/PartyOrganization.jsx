7ecd98d4bcf09c7b3db63537bdc0e156cbb55ff7
Alina Reabtev <alinareabteva@gmail.com>
2023-02-10 16:42:04 +0200

    tweaked readability of components

--- src/party/party_organization/PartyOrganization.jsx
+++ src/party/party_organization/PartyOrganization.jsx
@@ -7,24 +7,65 @@ const PartyOrganization = ({ partyOrganization, handleChange }) => {
     return (
         <>
             <div className="row">
-                <Input id="partyName" label="Party Name:" placeholder="Enter Party Name" type="text" value={partyOrganization.partyName} onChange={handleChange} />
+                <Input
+                    id="partyName"
+                    label="Party Name:"
+                    placeholder="Enter Party Name"
+                    type="text"
+                    value={partyOrganization.partyName}
+                    onChange={handleChange}
+                />
             </div>
 
             <div className="row organizer">
-                <Input id="firstName" label="Organizer:" placeholder="First Name" type="text" value={partyOrganization.firstName} onChange={handleChange} />
-                <Input id="lastName" placeholder="Last Name" type="text" value={partyOrganization.lastName} onChange={handleChange} />
+                <Input
+                    id="firstName"
+                    label="Organizer:"
+                    placeholder="First Name"
+                    type="text"
+                    value={partyOrganization.firstName}
+                    onChange={handleChange}
+                />
+                <Input
+                    id="lastName"
+                    placeholder="Last Name"
+                    type="text"
+                    value={partyOrganization.lastName}
+                    onChange={handleChange}
+                />
             </div>
 
             <div className="row">
-                <Input id="place" label="Place:" placeholder="Enter Place" type="text" value={partyOrganization.place} onChange={handleChange} />
+                <Input
+                    id="place"
+                    label="Place:"
+                    placeholder="Enter Place"
+                    type="text"
+                    value={partyOrganization.place}
+                    onChange={handleChange}
+                />
             </div>
 
             <div className="row">
-                <Input id="date" label="Date:" placeholder="Enter Date" type="date" value={partyOrganization.date} onChange={handleChange} />
+                <Input
+                    id="date"
+                    label="Date:"
+                    placeholder="Enter Date"
+                    type="date"
+                    value={partyOrganization.date}
+                    onChange={handleChange}
+                />
             </div>
 
             <div className="row">
-                <Input id="phoneNumber" label="Phone number:" placeholder="Phone Number" type="text" value={partyOrganization.phoneNumber} onChange={handleChange} />
+                <Input
+                    id="phoneNumber"
+                    label="Phone number:"
+                    placeholder="Phone Number"
+                    type="text"
+                    value={partyOrganization.phoneNumber} o
+                    nChange={handleChange}
+                />
             </div>
 
         </>
